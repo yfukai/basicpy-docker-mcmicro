378a595ef2c4f7c1854da5c79b3ad10c4e408499
Yohsuke Fukai <ysk@yfukai.net>
2023-01-16 17:23:56 +0900

    seems to be working?

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 VERSION=0.1.2
 docker buildx build \
+    --push \
     --platform linux/amd64,linux/arm64/v8 \
     -t yfukai/basicpy-docker-mcmicro:$VERSION \
     -t yfukai/basicpy-docker-mcmicro:latest \
     "."
 
 docker buildx build \
+    --push \
     --platform linux/amd64 \
     --build-arg BASE_IMAGE="nvidia/cuda:11.5.2-cudnn8-devel-ubuntu20.04" \
     --build-arg JAX_VERSION_EXTRA="cuda11_cudnn82" \
