b3c703f1cb8a6a10e7c0f4994ce1ec0669e63a18
Yohsuke Fukai <ysk@yfukai.net>
2023-04-23 11:27:27 +0900

    updated test script

--- test-cpu.sh
+++ test-cpu.sh
@@ -1,2 +1,6 @@
-docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:0.2.1 -d=cpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
+version="0.2.1"
+
+docker pull yfukai/basicpy-docker-mcmicro:${version}
+docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:${version} -d=cpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
+docker pull yfukai/basicpy-docker-mcmicro:latest
 docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:latest -d=cpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
--- test-gpu.sh
+++ test-gpu.sh
@@ -1,2 +1,6 @@
-docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:0.2.1-cuda -d=gpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
+version="0.2.1"
+
+docker pull yfukai/basicpy-docker-mcmicro:${version}-cuda
+docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:${version}-cuda -d=gpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
+docker pull yfukai/basicpy-docker-mcmicro:latest-cuda
 docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:latest-cuda -d=gpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
