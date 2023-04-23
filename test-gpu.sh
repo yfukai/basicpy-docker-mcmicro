a9bc533bb95cd0f27a2f0e021827a298c36eeb09
Yohsuke Fukai <ysk@yfukai.net>
2023-04-23 18:49:05 +0900

    fixed gpu test

--- test-gpu.sh
+++ test-gpu.sh
@@ -1,6 +1,6 @@
 version="0.2.1"
 
-docker pull yfukai/basicpy-docker-mcmicro:${version}-cuda
-docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:${version}-cuda -d=gpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
-docker pull yfukai/basicpy-docker-mcmicro:latest-cuda
-docker run --rm -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:latest-cuda -d=gpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
+docker pull yfukai/basicpy-docker-mcmicro:${version}
+docker run --rm --gpus all -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:${version} -d=cpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
+docker pull yfukai/basicpy-docker-mcmicro:latest
+docker run --rm --gpus all -v $(pwd)/testdata:/data yfukai/basicpy-docker-mcmicro:latest -d=cpu -i /data/exemplar-001-cycle-06.ome.tiff -o /data/
